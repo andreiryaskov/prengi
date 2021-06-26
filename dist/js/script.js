365631022483c07c92a6d3c5a4f32b8db2932125
andreiryaskov <andreiryaskov@gmail.com>
2021-06-26 11:34:32 +0900

    Prengi

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.decisions__slider').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      prevArrow: '<button type="button" class="slick-prev"><img class="decisions__slider-prev" src="img/slider_left.png" alt="<-"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img class="decisions__slider-next" src="img/slider_right.png" alt="->"></button>'
+    });
+});
+
+
